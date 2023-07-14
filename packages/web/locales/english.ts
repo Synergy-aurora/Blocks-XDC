85f57278fb7d8e12384b11303d1349c79f13a55e
Nwakakukaks <winnernwakaku123@gmail.com>
2023-07-14 23:19:57 +0000

    xdc branding

--- packages/web/locales/english.ts
+++ packages/web/locales/english.ts
@@ -3,8 +3,8 @@ export const en = {
     connectWalletTitle: 'Blocks › XDC',
     connectWalletSubtitle: 'Get connected to begin.',
     connectWalletDescription:
-      'You are one step away from accessing Blocks. Connect your MetaMask Wallet now to get started.',
-    connectButtonLabel: 'Connect MetaMask',
-    logoIcon: '/assets/icons/fantom-logo-round.svg',
+      'You are one step away from accessing Blocks. Connect your XDCPay Wallet now to get started.',
+    connectButtonLabel: 'Connect XDCPay',
+    logoIcon: '/assets/blocks/xdc.png',
   },
 };
