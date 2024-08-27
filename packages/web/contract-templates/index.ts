2194447c5964f9c0de036dc45973abd03e54ed68
nwakakukaks <winnernwakaku123@gmail.com>
2024-08-27 10:43:47 +0100

    add contracts

--- packages/web/contract-templates/index.ts
+++ packages/web/contract-templates/index.ts
@@ -1,11 +1,29 @@
 import templateCrowdSale from './templates/CrowdSale';
 import templateERC20 from './templates/ERC20';
 import templateERC721 from './templates/ERC721';
+import templateAuction from './templates/Auction';
+import templateCharityFund from './templates/CharityFund';
+import templateEscrow from './templates/Escrow';
+import templateMembershipManagement from './templates/MembershipManagement';
+import templateMultiSignature from './templates/MultiSignature';
+import templateRoyaltyDistribution from './templates/RoyaltyDistribution';
+import templateTimeLock from './templates/TimeLock';
+import templateTokenVesting from './templates/TokenVesting';
+import templateVoting from './templates/Voting';
 import templateOneTimeSubscription from './templates/OneTimeSubscription';
 
 export const TEMPLATES = [
   templateERC20,
   templateERC721,
   templateCrowdSale,
+  templateAuction,
+  templateCharityFund,
+  templateEscrow,
+  templateMembershipManagement,
+  templateMultiSignature,
+  templateRoyaltyDistribution,
+  templateTimeLock,
+  templateVoting,
+  templateTokenVesting,
   templateOneTimeSubscription,
 ];
